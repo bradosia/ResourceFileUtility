603ed3ef686aec7dc42c3ffd818b66f5f16ae25a
bradosia <bradosia@gmail.com>
2018-05-08 09:54:44 -0700

    save

--- README.md
+++ README.md
@@ -115,6 +115,40 @@ if(songInfo.inType == "FILE_FORMAT"){
 }
 ```
 
+### Compile Resources With Progress
+Pack all resources in your json file to a single resource file
+```cpp
+unsigned fileNumber, sizeTotal, sizeCurrent, loadBarNumber;
+ResourceFileUtility::Compiler RFUCompiler = new ResourceFileUtility::Compiler();
+RFUCompiler.info("resources.json");
+ResourceFileUtility::FileList fileList = RFUCompiler.getFileList();
+fileNumber = fileList.size();
+RFUCompiler.estimate();
+sizeTotal = RFUCompiler.sizePending();
+while (true) {
+	sizeCurrent = 0;
+	for (int i = 0; i < fileNumber; i++) {
+		sizeCurrent += fileList[i]->processSize();
+	}
+	loadBarNumber = (unsigned int) (sizeCurrent / (double) sizeTotal * 20.0);
+	cout << std::string(loadBarNumber, '#') << std::string(20 - loadBarNumber, ' ') << " Progress\r";
+	if (sizeCurrent >= sizeTotal)
+		break;
+}
+RFUCompiler.pack("assets.data");
+sizeTotal = RFUCompiler.sizePending();
+while (true) {
+	sizeCurrent = 0;
+	for (int i = 0; i < fileNumber; i++) {
+		sizeCurrent += fileList[i]->processSize();
+	}
+	loadBarNumber = (unsigned int) (sizeCurrent / (double) sizeTotal * 20.0);
+	cout << std::string(loadBarNumber, '#') << std::string(20 - loadBarNumber, ' ') << " Progress\r";
+	if (sizeCurrent >= sizeTotal)
+		break;
+}
+```
+
 
 ## C#
 add ```include/ResourceFileUtility.cs``` to your project<BR><BR>
--- example/src/main.cpp
+++ example/src/main.cpp
@@ -21,15 +21,42 @@ public:
 };
 
 int main() {
-	unsigned long long sizeCurrent, sizeTotal;
-	ResourceFileUtility::Compiler* RFUCompiler =
+	unsigned fileNumber, sizeTotal, sizeCurrent, loadBarNumber;
+	ResourceFileUtility::Compiler RFUCompiler =
 			new ResourceFileUtility::Compiler();
-	RFUCompiler->setCallback(new Callback());
-	RFUCompiler->info("resources.json");
-	RFUCompiler->packEstimate(sizeCurrent, sizeTotal);
-	RFUCompiler->pack("assets.data");
+	RFUCompiler.info("resources.json");
+	ResourceFileUtility::FileList fileList = RFUCompiler.getFileList();
+	fileNumber = fileList.size();
+	RFUCompiler.estimate();
+	sizeTotal = RFUCompiler.sizePending();
+	while (true) {
+		sizeCurrent = 0;
+		for (int i = 0; i < fileNumber; i++) {
+			sizeCurrent += fileList[i]->processSize();
+		}
+		loadBarNumber =
+				(unsigned int) (sizeCurrent / (double) sizeTotal * 20.0);
+		cout << std::string(loadBarNumber, '#')
+				<< std::string(20 - loadBarNumber, ' ') << " Progress\r";
+		if (sizeCurrent >= sizeTotal)
+			break;
+	}
+	RFUCompiler.pack("assets.data");
+	sizeTotal = RFUCompiler.sizePending();
+	while (true) {
+		sizeCurrent = 0;
+		for (int i = 0; i < fileNumber; i++) {
+			sizeCurrent += fileList[i]->processSize();
+		}
+		loadBarNumber =
+				(unsigned int) (sizeCurrent / (double) sizeTotal * 20.0);
+		cout << std::string(loadBarNumber, '#')
+				<< std::string(20 - loadBarNumber, ' ') << " Progress\r";
+		if (sizeCurrent >= sizeTotal)
+			break;
+	}
 
-	// Keep the console window open in debug mode.
+// Keep the console window open in debug mode.
 	string temp;
 	cout << "Press any key to exit." << endl;
 	getline(cin, temp);
