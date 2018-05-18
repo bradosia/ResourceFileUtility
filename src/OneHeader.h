968aa4f79b7269d0b4846eefc1634e270ad8cafa
bradosia <bradosia@gmail.com>
2018-05-17 23:21:16 -0700

    one header

---
+++ src/OneHeader.h
@@ -0,0 +1,8 @@
+#include "mingw_threads.h"
+#include "crc64.h"
+#include "json_fwd.h"
+#include "CallbackHandler.h"
+#include "ResourceFile.h"
+#include "Compiler.h"
+#include "Loader.h"
+#include "Stream.h"
